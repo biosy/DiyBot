b6f6eaed9bfec5de588109b991a28155bb15a9f5
biosy <aktor.alexis@hotmail.fr>
2017-10-23 11:32:39 +0200

    first basic protocol

--- src/lib/protocol/diyLink.cpp
+++ src/lib/protocol/diyLink.cpp
@@ -1,1 +1,11 @@
 #include "diyLink.h"
+
+DiyLink::DiyLink(uint8_t msgid_t, uint8_t endmsg_t, uint16_t payload1_t, uint16_t payload2_t, uint16_t payload3_t): msgid(msgid_t), endmsg(endmsg_t), payload1(payload1_t), payload2(payload2_t), payload3(payload3_t){
+
+}
+
+char* DiyLink::bufferize(){
+	char* buffer = (char*)malloc(16*sizeof(char));
+	return buffer;
+
+}
--- src/lib/protocol/diyLink.h
+++ src/lib/protocol/diyLink.h
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
-class diyLink{
+class DiyLink{
 	private:
 		uint8_t msgid;
-		uint8_t size;
 		uint8_t endmsg;
 		uint16_t payload1;
 		uint16_t payload2;
 		uint16_t payload3;
 
+	public:
+		char* bufferize();
+		DiyLink(uint8_t msgid_t, uint8_t endmsg_t, uint16_t payload1_t, uint16_t payload2_t, uint16_t payload3_t);
 
 };
