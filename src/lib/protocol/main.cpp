576d6c475230ee6eed62a400dc9c1e7792802cd4
biosy <aktor.alexis@hotmail.fr>
2017-11-07 10:41:07 +0100

    add status message and implemented

--- src/lib/protocol/main.cpp
+++ src/lib/protocol/main.cpp
@@ -7,8 +7,15 @@ using namespace std;
 
 int main(){
 
+	
 	DiyLink* d = new DiyLink(200,00,0010,0110, 0030);
 	d->print();
+
+	uint8_t x = 0x01;
+	uint8_t y = 0x02;
+
+	Status* status= new Status(x,y);
+	status->bufferize();
 	return 0;
 }
 
